952a6d53cecb27998ac4ba5cb3654ec9037fdd5e
Dev Sharma <ginnisharma1972000@gmail.com>
2024-09-26 23:51:47 +0530

    fix

--- Backend/src/controller/employee/get-all-employee.controller.js
+++ Backend/src/controller/employee/get-all-employee.controller.js
@@ -1,34 +1,32 @@
 const employeeModel = require('../../models/employee');
 const log = require('lambda-log');
 
-const getAllEmployees = async (req, res, next) => {
-    // const data = req.user;
-    // log.info("Fetching Employee List", { "employeeId": data._id, "firstName": data.firstName, "file": "get-all-Employee.controller.js", "method": "getAllEmployee" });
+const getAllEmployees = async(req,res,next)=>{
+    const data = req.user;
+    log.info("Fetching Employee List" , {"employeeId" : data._id ,"firstName":data.firstName , "file": "get-all-Employee.controller.js" , "method": "getAllEmployee"});
     try {
-        // let empData;
-        let data = await employeeModel.find();
-        // if(data.role === 'superadmin'){
-        //     empData = await employeeModel.find({}, 
-        //         "firstName lastName username role active profileImage reportStats leaving_date joining_date shift terminate_reason hasPolicyAccepted  itPolicyAcceptance biometric_id employee_category phoneNumber opfin_id official_email dob allocated_under.TeamLeadName appraisal_month weekly_off face_auth"
-        //     );
-        // }
-        // if(data.role === 'teamleader'){
-        //     empData = await employeeModel.find({
-        //         _id:{$ne : data._id}, role: {$nin :["superadmin"]}
-        //     }, 
-        //         "firstName lastName username role active profileImage reportStats leaving_date joining_date shift terminate_reason hasPolicyAccepted  itPolicyAcceptance biometric_id employee_category phoneNumber opfin_id official_email dob allocated_under.TeamLeadName appraisal_month weekly_off face_auth"
-        //     );
-        // }
-        // if(data.role === 'employee'){
-        //     empData = await employeeModel.find({
-        //         _id:{$ne : data._id}, role: {$nin :["superadmin", "teamleader", "System Support"]}
-        //     }, 
-        //         "firstName lastName role joining_date shift"
-        //     );
-        // }
-
-        // log.info("Employee List Fetched Successfully", { "employeeId": data._id, "firstName": data.firstName, "file": "get-all-Employee.controller.js", "method": "getAllEmployee" });
-        return res.status(200).send({ message: "Employees data Fetched Successfully", data })
+        let empData;
+        if(data.role === 'superadmin'){
+            empData = await employeeModel.find({}, 
+                "firstName lastName username role active profileImage reportStats leaving_date joining_date shift terminate_reason hasPolicyAccepted  itPolicyAcceptance biometric_id employee_category phoneNumber opfin_id official_email dob allocated_under.TeamLeadName appraisal_month weekly_off face_auth"
+            );
+        }
+        if(data.role === 'teamleader'){
+            empData = await employeeModel.find({
+                _id:{$ne : data._id}, role: {$nin :["superadmin"]}
+            }, 
+                "firstName lastName username role active profileImage reportStats leaving_date joining_date shift terminate_reason hasPolicyAccepted  itPolicyAcceptance biometric_id employee_category phoneNumber opfin_id official_email dob allocated_under.TeamLeadName appraisal_month weekly_off face_auth"
+            );
+        }
+        if(data.role === 'employee'){
+            empData = await employeeModel.find({
+                _id:{$ne : data._id}, role: {$nin :["superadmin", "teamleader", "System Support"]}
+            }, 
+                "firstName lastName role joining_date shift"
+            );
+        }
+        log.info("Employee List Fetched Successfully" , {"employeeId" : data._id ,"firstName":data.firstName , "file": "get-all-Employee.controller.js" , "method": "getAllEmployee"});
+        return res.status(200).send({message: "Employees data Fetched Successfully" , empData})
     } catch (err) {
         log.error(err, "Error Occured while Fetching employees from DB");
         let error = new Error(`Error Occured While Fetcching Employees - ${err.message}`);
