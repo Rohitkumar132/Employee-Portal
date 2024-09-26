078c0abae3a305a02e3ffa33cdc0f17accd947ea
Rohitkumar132 <954111rohit@gmail.com>
2024-09-26 18:40:35 +0530

    Fix Schema issues

--- Backend/src/controller/stores/index.js
+++ Backend/src/controller/stores/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const storeController = express.Router();
 const addCustomer = require('./add-store.controller');  
 
-storeController.post('/add-customer' ,addCustomer );
+storeController.post('/add-customer', addCustomer);
+
+module.exports = storeController;
--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./../controller/users/index');
-const employeeController = require('./../controller/register/index');
+const employeeController = require('./../controller/employee/index');
+const storeController = require('./../controller/stores/index')
 
 router.use('/' , userController );
 router.use('/employee' ,employeeController);
+router.use('/store' , storeController)
 
 module.exports = router;
